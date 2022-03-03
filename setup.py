fe4e7d3588eeabf710d67c61052243bd87022515
Phil Wang <lucidrains@gmail.com>
2022-03-03 08:07:56 -0800

    scaffold

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'memory-efficient-attention-pytorch',
+  packages = find_packages(exclude=[]),
+  version = '0.0.1',
+  license='MIT',
+  description = 'Memory Efficient Attention - Pytorch',
+  author = 'Phil Wang',
+  author_email = 'lucidrains@gmail.com',
+  url = 'https://github.com/lucidrains/memory-efficient-attention-pytorch',
+  keywords = [
+    'artificial intelligence',
+    'deep learning',
+    'attention-mechanism'
+  ],
+  install_requires=[
+    'torch>=1.6'    
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
