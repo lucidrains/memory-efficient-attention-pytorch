50e06b8647e94f1db503bad62dc33e7880d03bd2
Phil Wang <lucidrains@gmail.com>
2022-07-23 13:00:43 -0700

    oops, actually add cosine sim flash attention

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'memory-efficient-attention-pytorch',
   packages = find_packages(exclude=[]),
-  version = '0.0.20',
+  version = '0.0.22',
   license='MIT',
   description = 'Memory Efficient Attention - Pytorch',
   long_description_content_type = 'text/markdown',
